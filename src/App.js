7b86af996f81977e852c73645439b62f57bafcf8
Sriabhi Venkat <65041220+sriabhivenkat@users.noreply.github.com>
2022-06-24 16:24:32 -0500

    d

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
         </div>
         <div className="percentRemaining">
           <div className="countdown-item">
-            <div className="percentRemainingNumber">{(remainingTime?.weeks/8)*100}% remaining in the trip</div>
+            <div className="percentRemainingNumber">{(remainingTime?.weeks/8)*100}% of the trip left</div>
           </div>
         </div>
       </header>
